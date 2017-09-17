bfe9d5e32232c92a6ddcb4265297b5bd2ab57f54
Maxim Surovtsev <realitybelieves@gmail.com>
2017-09-17 17:42:22 +0300

    Simple grid layout. Navigation bar.

---
+++ Guardfile
@@ -0,0 +1,4 @@
+# github.com/guard/guard-livereload
+guard 'livereload' do
+  watch(%r{.+\.(css|js|html)})
+end
---
+++ script.js
@@ -0,0 +1,24 @@
+// var addNewItem = function(ob) {
+//   var content = document.getElementById("content");
+//   var item = document.createElement("div");
+//   item.id = "item"
+//   item.innerHTML = 
+//     `<table>
+//       <tr>
+//         <td>IMG</td>
+//         <td>${ob.name}</td>
+//         <td>${ob.old_price}</td>
+//         <td>${ob.new_price}</td>
+//         <td>${ob.discount}</td>
+//       </tr>
+//     </table>`
+//   content.appendChild(item);
+// }
+
+
+// var jsonString = '{"name":"Max","old_price":"500$","new_price":"600$","discount":"40%"}'
+// var data = JSON.parse(jsonString)
+// addNewItem(data);
+
+// // http://95.129.137.5/project/dixy_items.json
+
