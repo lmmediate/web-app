792f6a02dcf9af856cac3e01863b80742fb55ba2
Maxim Surovtsev <realitybelieves@gmail.com>
2017-09-19 02:14:42 +0300

    Trying to show crawled data.

--- script.js
+++ script.js
@@ -4,33 +4,32 @@ var addNewItem = function(ob) {
   item.className = "item";
   item.innerHTML = 
     `<div class="item-name">${ob.name}</div>
-     <div class="item-img"><img src="https://dixy.ru/upload/iblock/445/%D0%A6%D0%A20027448.jpg"/></div>
-     <div class="item-cat">${ob.cat}</div>
+     <div class="item-img"><img src="${ob.img_url}"/></div>
+     <div class="item-cat">${ob.category}</div>
      <div class="item-old-price">${ob.old_price}</div>
      <div class="item-new-price">${ob.new_price}</div>
      <div class="item-discount">${ob.discount}</div>
      <div class="item-date">${ob.date}</div>
-     <div class="item-cond">Условие</div>`;
+     <div class="item-cond">${ob.condition}</div>`;
   itemList[0].appendChild(item);
 }
 
 
-var jsonString = '{"name":"Груша зеленая 1кг","cat":"Овощи и фрукты","old_price":"69.50","new_price":"600$","discount":"-40%","date":"11.09-18.09"}';
-var data = JSON.parse(jsonString);
-addNewItem(data);
-addNewItem(data);
-addNewItem(data);
-addNewItem(data);
-addNewItem(data);
-addNewItem(data);
-addNewItem(data);
+// var jsonString = '{"name":"Груша зеленая 1кг","category":"Овощи и фрукты","old_price":"69.50","new_price":"600$","discount":"-40%","date":"11.09-18.09"}';
+// var data = JSON.parse(jsonString);
+// addNewItem(data);
 
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        var myArr = JSON.parse(this.responseText);
-        console.log(myArr[0]);
+    data = JSON.parse(this.responseText);
+    for (i in data) {
+      addNewItem(data[i]);
     }
+    // console.log(data);
+  }
 };
 xmlhttp.open("GET", "../server/dixy_items.json", true);
-xmlhttp.send(); 
+xmlhttp.send();
+
+
