89a67b9430af732abd368d89823a299ed9cabe9d
Maxim Surovtsev <realitybelieves@gmail.com>
2017-09-18 17:51:22 +0300

    Discounts section advance layout.

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ var addNewItem = function(ob) {
   var item = document.createElement("div");
   item.className = "item";
   item.innerHTML = 
-    `<div class="item-img"><img src="https://dixy.ru/upload/iblock/445/%D0%A6%D0%A20027448.jpg"/></div>
-     <div class="item-name"${ob.name}></div>
+    `<div class="item-name">${ob.name}</div>
+     <div class="item-img"><img src="https://dixy.ru/upload/iblock/445/%D0%A6%D0%A20027448.jpg"/></div>
      <div class="item-cat">${ob.cat}</div>
      <div class="item-old-price">${ob.old_price}</div>
      <div class="item-new-price">${ob.new_price}</div>
      <div class="item-discount">${ob.discount}</div>
      <div class="item-date">${ob.date}</div>
-     <div class="item-cond">-</div>`;
+     <div class="item-cond">Условие</div>`;
   itemList[0].appendChild(item);
 }
 
 
-var jsonString = '{"name":"Груша зеленая 1 кг","cat":"Овощи и фрукты","old_price":"99.99","new_price":"600$","discount":"-","date":"date"}';
+var jsonString = '{"name":"Груша зеленая 1кг","cat":"Овощи и фрукты","old_price":"69.50","new_price":"600$","discount":"-40%","date":"11.09-18.09"}';
 var data = JSON.parse(jsonString);
+console.log(data);
 addNewItem(data);
 addNewItem(data);
 addNewItem(data);
