4fe0a30e0bda2c9bdd45fc993ef7dbc9ebc380b9
Maxim Surovtsev <realitybelieves@gmail.com>
2017-11-25 19:49:47 +0300

    Item search.

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
 // var url = 'http://46.17.44.125:8080/api/sales';
 var url = 'http://localhost:8080/api';
 
-var vm;
+var vm = new Vue({
+  el: '#grid',
+  data: {
+    items: [],
+    itemsFiltered: []
+  }, 
+  methods: {
+    total: function() {
+      return this.itemsFiltered.length;
+    },
+    filter: function(event) {
+      var name = event.target.value;
+      console.log(event);
+      // TODO: call a filter function from API
+      // to filter all items (not a single page)
+      this.itemsFiltered = this.items.filter(function(value) {
+        return value.name.toLowerCase().indexOf(name.toLowerCase()) !== -1;
+      });
+    },
+  }
+});;
 
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open('GET', url + '/sales/page/1');
+xmlhttp.open('GET', url + '/sales/page/2');
 xmlhttp.onload = function() {
   if (xmlhttp.status === 200) {
-    var d = JSON.parse(xmlhttp.responseText);
-
-    vm = new Vue({
-      el: '#item-list',
-      data: {
-        items: d
-      }
-    });
-
-    console.log(vm.items);
-
+    var data = JSON.parse(xmlhttp.responseText);
+    vm.items = data;
+    vm.itemsFiltered = data;
   } else {
     console.log('Loading error.');
   }
