4f0b45c87373130ce8f0f6c556fc3804fe512b82
Maxim Surovtsev <realitybelieves@gmail.com>
2018-01-03 16:32:49 +0300

    Header component.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,58 +1,26 @@
 <template>
-    <div id="header">
-      <div id="logo"><router-link to="/">ShopDisounts</router-link></div>
-      <div id="top-navbar">
-        <ul>
-          <li><router-link to="/discounts">Акции</router-link></li>
-          <li><router-link to="/shoplist">Список покупок</router-link></li>
-        </ul>
-      </div>
-    </div>
+  <div id="header">
+    <b-navbar toggleable="md" type="dark" variant="info">
+
+      <b-navbar-brand to="/">ShopDiscounts</b-navbar-brand>
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item to="/discounts">Акции</b-nav-item>
+          <b-nav-item to="/shoplist">Список покупок</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+
+    </b-navbar>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {};
-  }
 }
 </script>
 
 <style>
-div#header {
-  background-color: rgb(204, 153, 255);
-}
-div#logo {
-  padding: 10px;
-  text-align: center;
-  font-size: 180%;
-}
-div#top-navbar {
-  font-size: 120%;
-  text-align: center;
-}
-div#top-navbar ul {
-  list-style-type: none;
-}
-a {
-  text-decoration: none;
-}
-a:visited {
-  color: black;
-}
-div#top-navbar a {
-  display: inline-block;
-  padding: 10px;
-  text-align: center;
-  color: black;
-}
-div#top-navbar a:hover {
-  background-color: rgb(153, 51, 255);
-  color: white;
-}
-@media screen and (min-width: 768px) {
-  div#top-navbar li {
-     display: inline-block;
-  }
-}
 </style>
