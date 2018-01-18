a0c1be4d62ee8f04b8f0911ab301fbfca0a5fc6d
Maxim Surovtsev <realitybelieves@gmail.com>
2018-01-18 12:25:50 +0300

    Deleted ItemList component, added search input.

--- src/components/Discounts.vue
+++ src/components/Discounts.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="discounts">
-    <item-list v-bind:items="items"></item-list>
-    <pagination v-bind:info="info"></pagination>
+    <b-container fluid>
+      <b-row>
+        <b-col cols="12" md="6" lg="4">
+          <b-form-input type="search" placeholder="Поиск..."></b-form-input>
+        </b-col>
+      </b-row> 
+      <b-row>
+        <b-col 
+          cols="12" md="6" lg="4" xl="3"
+          class="py-2 border"
+          v-for="item in items"
+          v-bind:key="item.name">
+            <item class="mx-auto" v-bind:item="item" v-bind:key="item.id"></item>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <pagination v-bind:info="info"></pagination>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import ItemList from './ItemList.vue'
 import Pagination from './Pagination.vue'
+import Item from './Item.vue'
 
 export default {
   components: {
-    'item-list': ItemList,
-    'pagination': Pagination
+    'pagination': Pagination,
+    'item': Item'
   },
   data: function() {
     return {
