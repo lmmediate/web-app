9f96be7643fe62d6681f1f334fb70384a6311738
Maxim Surovtsev <realitybelieves@gmail.com>
2018-01-18 16:37:46 +0300

    Promises to chain http requests, front-end pagination, remove pagination component.

--- src/components/Discounts.vue
+++ src/components/Discounts.vue
@@ -10,14 +10,20 @@
         <b-col 
           cols="12" md="6" lg="4" xl="3"
           class="py-2 border"
-          v-for="item in items"
+          v-for="item in filteredItems"
           v-bind:key="item.name">
             <item class="mx-auto" v-bind:item="item" v-bind:key="item.id"></item>
         </b-col>
       </b-row>
       <b-row>
         <b-col>
-          <pagination v-bind:info="info"></pagination>
+          <div id="pagination">
+            <b-pagination-nav
+              use-router
+              base-url="/discounts/" 
+              v-bind:number-of-pages="info.numPages" 
+              v-model="currentPage" />
+          </div>
         </b-col>
       </b-row>
     </b-container>
@@ -25,42 +31,45 @@
 </template>
 
 <script>
-import Pagination from './Pagination.vue'
 import Item from './Item.vue'
 
 export default {
   components: {
-    'pagination': Pagination,
     'item': Item
   },
   data: function() {
     return {
       info: {},
-      items: [] 
+      items: [],
+      filteredItems: [],
+      currentPage: 1
     }
   },
   methods: {
-    getInfo: function() {
-      var req = 'sales/info';
-      this.$http.get(req).then(res => {
-        this.info = res.data;
-      }, res => {
-        console.log('Error during GET request. Url: ' + req);
-      });
-    },
-    getItems: function() {
-      var req = 'sales';
-      this.$http.get(req).then(res => {
-        this.items = res.data;
-      }, res => {
-        console.log('Error during GET request. Url: ' + req);
-      });
-    },
+    getPage: function(num) {
+      this.filteredItems = 
+        this.items.slice(this.info.itemsPerPage * (+num - 1), 
+          this.info.itemsPerPage * +num);
+	console.log(this.items);
+        console.log(this.filteredItems);
+    }
   },
   beforeMount: function() {
-    this.getInfo();
-    this.getItems();
-  }
+    this.$http.get('sales/info')
+      .then(res => {
+        this.info = res.data;
+        return this.$http.get('sales');
+      })
+      .then(res => {
+        this.items = res.data;
+        this.getPage(1);
+      });
+  },
+  watch: {   
+      '$route': function(to, from) {
+      this.getPage(to.params.page);
+    }
+  },
 }
 </script>
 
