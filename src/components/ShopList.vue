86da5b9781c0ee34bd923292c5a724c805a629f0
Maxim Surovtsev <realitybelieves@gmail.com>
2018-02-13 10:25:23 +0300

    Show basic shoplist.

--- src/components/ShopList.vue
+++ src/components/ShopList.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
   	<h2 style="margin: 10px; font-weight: lighter">Список покупок</h2>
+    <ul>
+      <li v-for="item in items">{{item.name}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-    
+  data: function() {
+    return {
+      items: [] 
+    }
+  },
+  beforeMount: function() {
+    this.$http.get('shoplist')
+      .then(res => {
+        this.items = res.data;
+      });
+  }
 }
 </script>
-
-<style>
-
-</style>
