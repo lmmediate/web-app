1b0843ac2686d7ba8b5182c67e44c74a0d19243e
Maxim Surovtsev <realitybelieves@gmail.com>
2018-02-13 22:37:14 +0300

    ShopListItem - new component.

---
+++ src/components/ShopListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="shoplist-item">
+    <div class="shoplist-item-img">
+      <img v-bind:src="item.imageUrl"/>
+    </div>
+    <div class="shoplist-item-name">{{displayName}}</div>
+    <div class="shoplist-item-price-container">
+      <div class="shoplist-item-old-price">{{item.oldPrice}}&#8381;</div>
+      <div class="shoplist-item-new-price">{{item.newPrice}}&#8381;</div>
+    </div>
+    <b-btn class="remove-from-shoplist-btn" v-on:click="removeFromShopList">e</b-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  methods: {
+    removeFromShopList: function() {
+
+    }
+  }
+}
+</script>
