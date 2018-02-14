95ecec0b5ef32371c79897a905b7047018d15a5a
Maxim Surovtsev <realitybelieves@gmail.com>
2018-02-14 21:53:54 +0300

    ShopListItem styling.

--- src/components/ShopListItem.vue
+++ src/components/ShopListItem.vue
@@ -3,17 +3,25 @@
     <div class="item-img">
       <img v-bind:src="item.imageUrl"/>
     </div>
-    <div class="item-name">{{item.name}}</div>
-    <div class="item-price-container">
-      <div class="item-old-price">{{item.oldPrice}}&#8381;</div>
-      <div class="item-new-price">{{item.newPrice}}&#8381;</div>
+    <div class="item-desc">
+      <div class="item-name">{{displayName}}</div>
+      <div class="item-price-container">
+        <div class="item-new-price">{{item.newPrice}}&#8381;</div>
+        <div class="item-old-price">{{item.oldPrice}}&#8381;</div>
+      </div>
     </div>
     <b-btn class="remove-from-shoplist-btn" v-on:click="removeFromShopList">-</b-btn>
   </div>
 </template>
 
+
 <script>
 export default {
+  data: function() {
+    return {
+      maxNameLength: 80
+    }
+  },
   props: {
     item: {
       type: Object,
@@ -29,15 +37,26 @@ export default {
           this.$emit('removeFromShopList', this.index);
         });
     }
+  },
+  computed: {
+    displayName: function() {
+      if(this.item.name.length <= this.maxNameLength){
+        return this.item.name;
+      } else {
+        return this.item.name.slice(0, this.maxNameLength) + '...';
+      }
+    }
   }
 }
 </script>
 
 <style scoped>
 div.item {
-  width: 450px;
+  position: relative;
+  width: 100%;
   height: 150px;
   padding: 5px;
+
   background-color: white;
   box-shadow: 3px 3px 15px -5px rgba(0,0,0,0.5);
 }
@@ -51,5 +70,20 @@ div.item-img {
 div.item-img > img {
   width: 100%;
   height: 100%;
+}
+div.item-desc {
+}
+div.item-old-price {
+  color: rgb(97, 97, 97);
+  text-decoration: line-through;
+}
+div.item-new-price {
+  font-size: 1.5em;
+}
+.remove-from-shoplist-btn {
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  padding: 10px;
 }
 </style>
