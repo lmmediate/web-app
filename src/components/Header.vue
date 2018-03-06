b75676dc78ff193c2436250d9ce66e46f854efcb
Maxim Surovtsev <realitybelieves@gmail.com>
2018-03-06 10:14:49 +0300

    Header login/logout link.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,14 +11,43 @@
           <b-nav-item to="/discounts">Акции</b-nav-item>
           <b-nav-item to="/shoplist">Список покупок</b-nav-item>
         </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item v-if="!isLoggedIn" to="/login">Войти</b-nav-item>
+          <b-nav-item v-if="isLoggedIn" v-on:click="logout">Выйти</b-nav-item>
+        </b-navbar-nav>
       </b-collapse>
-
     </b-navbar>
   </div>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      isLoggedIn: false
+    }
+  }, 
+  methods: {
+    logout: function() {
+      localStorage.removeItem('auth');
+      console.log('logout!');
+      this.isLoggedIn = false;
+      this.$router.push('/');
+    }
+  },
+  beforeMount: function() {
+    this.$http.get('api/shoplist', { 
+      headers: {
+        'Authorization': localStorage.getItem('auth')
+      }
+    })
+      .then(() => {
+        this.isLoggedIn = true;
+      })
+      .catch(() => {
+        this.isLoggedIn = false; 
+      });
+  }
 }
 </script>
 
