e91e4d38668efe8195f2fb4fb42b27ae52c9ef48
Maxim Surovtsev <realitybelieves@gmail.com>
2018-03-10 01:42:24 +0300

    ItemSmall.vue.

---
+++ src/components/ItemSmall.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="item">
+    <div class="item-main-info">
+      <div class="item-img">
+        <img v-bind:src="item.imageUrl"/>
+      </div>
+      <div class="item-description">
+        <div class="item-name">{{displayName}}</div>
+        <div class="item-category">{{item.category}}</div>
+        <div class="item-old-price">{{item.oldPrice}}&#8381;</div>
+        <div class="item-new-price">{{item.newPrice}}&#8381;</div>
+        <div class="item-discount">{{item.discount}}%</div>
+      </div>
+    </div>
+    <div class="item-conditions">
+      <!-- <div class="item-date">{{item.dateIn}} - {{item.dateOut}}</div> -->
+      <!-- <div class="item-cond">{{item.condition}}</div> -->
+    </div>
+    <b-btn class="add-to-shoplist-btn">+</b-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  //props: ['item']
+  data: function() {
+    return {
+      item: {
+        "name": "Лук репчатый красный, 1 кг Лук репчатый красный, 1 кг Лук репчатый красный, 1 кг Лук репчатый красный, 1 кг Лук репчатый, 1 кг",
+        "category": "Овощи и фрукты", 
+        "imageUrl": "https://dixy.ru/upload/iblock/289/2000003216.jpg", 
+        "oldPrice": 47.1,
+        "newPrice": 39.99,
+        "discount": "-15", 
+        "dateIn": "2018-03-05",
+        "dateOut": "2018-03-11", 
+        "crawlDate": "2018-03-05", 
+        "condition": "-"
+      },
+      maxNameLength: 40
+    }
+  }, 
+  computed: {
+    displayName: function() {
+      if(this.item.name.length <= this.maxNameLength){
+        return this.item.name;
+      } else {
+        return this.item.name.slice(0, this.maxNameLength) + '...';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.item {
+  padding: 5px;
+  background-color: white;
+  width: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.item-main-info {
+  display: flex;
+  flex-direction: row;
+}
+.item-category {
+  color: rgb(155, 155, 155);
+  font-size: 0.8em;
+  font-weight: 600;
+}
+.item-img > img {
+  width: 100%;
+  height: 100%;
+}
+.item-img {
+  height: 130px;
+  width: 130px;
+  margin-right: 5px;
+}
+.item-conditions {
+  align-self: flex-end;
+}
+.item-old-price {
+  color: rgb(97, 97, 97);
+  text-decoration: line-through;
+  font-size: 0.8em;
+}
+.item-new-price {
+  font-weight: 600;
+}
+.item-discount {
+  padding: 2px;
+  border-radius: 5px;
+  background-color: rgb(244, 209, 66);
+  display: inline-block;
+}
+</style>
