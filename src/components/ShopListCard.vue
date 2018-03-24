4d84a7a464f690304e0d3413125437e631da2760
Maxim Surovtsev <realitybelieves@gmail.com>
2018-03-24 07:55:41 +0300

    ShopListCard and ShopListPreview components init.

---
+++ src/components/ShopListCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-card 
+    @mouseover="onMouseOver"
+    @mouseout="onMouseOut"
+    @click="onClick" 
+    :header-bg-variant="headerVariant.bg"
+    :header-text-variant="headerVariant.fg"
+    :header="shoplist.name">
+    <p class="card-text">
+    <b-list-group>
+      <b-list-group-item v-for="item in shoplist.items">
+        {{item.name}}
+      </b-list-group-item>
+    </b-list-group>
+    </p>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: ['shoplist'],
+  data: function() {
+    return {
+      headerVariant: {
+        bg: 'secondary',
+        fg: 'white'
+      },
+      maxNameLength: 40
+    }
+  },
+  computed: {
+    // TODO: displayName
+  },
+  methods: {
+    onMouseOver: function() {
+      this.headerVariant.bg = 'light';
+      this.headerVariant.fg = 'black';
+    },
+    onMouseOut: function() {
+      this.headerVariant.bg = 'secondary';
+      this.headerVariant.fg = 'white';
+    },
+    onClick: function() {
+      console.log('CLICK');
+    },
+    openShopList: function() {
+      
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ShopListPreview.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <b-container fluid>
+      <b-row class="mt-2">
+        <b-col cols="12" md="6" lg="4" xl="3"
+          class="mb-2" 
+          v-for="shoplist in shoplists">
+          <shoplist-card :shoplist="shoplist"></shoplist-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import ShopListCard from './ShopListCard.vue'
+
+export default {
+  components: {
+    'shoplist-card': ShopListCard
+  },
+  data: function() {
+    return {
+      shoplists: [{
+        name: 'Завтрак',
+        items: ['ССокссссссссссссссссссссссссссссссссссссссссссоксссссссссссссссссссссссссссссссссссссссссс', 'Вода', 'Молоко'] 
+      },{
+        name: 'Завтрак',
+        items: ['Сок', 'Вода', 'Молоко'] 
+      },{
+        name: 'Завтрак',
+        items: ['Сок', 'Вода', 'Молоко'] 
+      },{
+        name: 'Завтрак',
+        items: ['Сок', 'Вода', 'Молоко'] 
+      },{
+        name: 'Завтрак',
+        items: ['Сок', 'Вода', 'Молоко'] 
+      },{
+        name: 'Завтрак',
+        items: ['Сок', 'Вода', 'Молоко'] 
+      },{
+        name: 'Завтрак',
+        items: ['Сок', 'Вода', 'Молоко'] 
+      },{
+        name: 'Завтрак',
+        items: ['Сок', 'Вода', 'Молоко'] 
+      },{
+        name: 'Завтрак',
+        items: ['Сок', 'Вода', 'Молоко'] 
+      }]
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+</style>
