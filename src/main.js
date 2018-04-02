a25b37ad7fdad463dd30387f840a1092e9ae7f9c
Maxim Surovtsev <realitybelieves@gmail.com>
2018-04-02 22:07:19 +0300

    Add sass dependencies, remove boostrap css.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
