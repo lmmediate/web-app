1faec443639651d9aa9385e5ef9690d62ef33090
Maxim Surovtsev <realitybelieves@gmail.com>
2018-05-20 23:40:24 +0300

    main.js
    Change route paths, get shops on app start.

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import ShopList from './components/ShopList.vue'
 import ShopListPreview from './components/ShopListPreview.vue'
 import Login from './components/Login.vue'
 import Register from './components/Register.vue'
+import ShopSelector from './components/ShopSelector.vue'
 import auth from './auth'
 
 Vue.use(BootstrapVue)
@@ -19,42 +20,50 @@ Vue.use(VueRouter);
 // Check user login on the app start
 auth.checkLogin();
 
+// function beforeEnter(to, from, next) {
+//   // Custom redirect function
+//   next({path: '/sales', query: {shop: 1}});
+// }
+
+
+Vue.http.options.root = 'http://gcsales.ru';
+Vue.http.interceptors.push(function (req, next) {
+  var token = localStorage.getItem('auth');
+  req.headers.set('Authorization', token);
+  next();
+});
+
+// Get available shops and store them in localstorage
+Vue.http.get('api/shops')
+  .then(res => {
+    var jsonData = JSON.stringify(res.data);
+    localStorage.setItem('shops', jsonData);
+  });
+
+
 function passProps(route) {
   return {
-    shopId: route.query.shop,
-    currentCategory: route.query.category,
-    currentPage: route.query.page 
+    shop: route.params.shop,
+    category: route.query.category
   }
 }
 
 const routes = [
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
-  { path: '/', beforeEnter: (to, from, next) => {
-    // Custom redirect function
-    next({ path: '/sales', query: { shop: 1, page: 1 } });
-  }},
-  { path: '/sales', component: Discounts, props: passProps  },
-  { path: '/shoplist', component: ShopListPreview },
-  { path: '/shoplist/:id', component: ShopList }
+  {path: '/', redirect: '/sales'},
+  {path: '/sales', component: ShopSelector},
+  {path: '/sales/:shop', component: Discounts, props: passProps},
+  {path: '/shoplist', component: ShopListPreview},
+  {path: '/shoplist/:id', component: ShopList},
+  {path: '/login', component: Login},
+  {path: '/register', component: Register}
 ];
 
 const router = new VueRouter({
   routes: routes
 });
 
-// 'http://46.17.44.125:8080',
-// 'http://localhost:8080',
- 
-Vue.http.options.root = 'http://gcsales.ru'; 
-Vue.http.interceptors.push(function(req, next) {
-  var token = localStorage.getItem('auth');
-  req.headers.set('Authorization', token);
-  next();
-});
-
 new Vue({
   el: '#app',
   router: router,
   render: h => h(App)
-})
+});
