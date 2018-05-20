613166a51f0cc316705c7fe5cf1b8b5682a90ed3
Maxim Surovtsev <realitybelieves@gmail.com>
2018-05-20 23:43:29 +0300

    New ShopSelector.vue component.

---
+++ src/components/ShopSelector.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-container fluid>
+    <b-row class="mt-2">
+      <b-col cols="12" md="6" lg="4" xl="3"
+             v-for="shop in shops" :key="shop.id">
+        <b-card>
+          <p class="card-text">
+            <router-link :to="'/sales/' + shop.alias">{{shop.name}}</router-link>
+          </p>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        shops: JSON.parse(localStorage.getItem('shops'))
+      }
+    },
+  }
+</script>
